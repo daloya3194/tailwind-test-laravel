88e44545378644111c249925c7ed82b37df9d3cc
Danick Loic Yagoue <dy@alphabytes.de>
2023-01-17 14:26:42 +0100

    custom radio button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -119,9 +119,77 @@
                 {{--            <div class="swiper-pagination"></div>--}}
             </div>
 
-
             <br>
             <a href="{{ route('get-pdf') }}" target="_blank">Get PDF</a>
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="mx-auto max-w-6xl px-12">
+                <div class="flex flex-wrap gap-3">
+
+                    <label class="cursor-pointer">
+                        <input type="radio" class="peer sr-only" name="pricing" />
+                        <div class="w-72 max-w-xl rounded-md bg-white p-5 text-gray-600 ring-2 ring-transparent hover:shadow peer-checked:text-sky-600 peer-checked:ring-sky-400 peer-checked:ring-offset-2">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex items-center justify-between">
+                                    <p class="text-sm font-semibold uppercase text-gray-500">essential</p>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6">
+                                            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="flex items-center justify-between">
+                                    <p><span class="text-lg font-bold">12 GB</span> of storage</p>
+                                    <p class="text-sm font-bold">$12 / mo</p>
+                                </div>
+                            </div>
+                        </div>
+                    </label>
+
+                    <label class="cursor-pointer">
+                        <input type="radio" class="peer sr-only" name="pricing" />
+                        <div class="w-72 max-w-xl rounded-md bg-white p-5 text-gray-600 ring-2 ring-transparent hover:shadow peer-checked:text-sky-600 peer-checked:ring-sky-400 peer-checked:ring-offset-2">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex items-center justify-between">
+                                    <p class="text-sm font-semibold uppercase text-gray-500">essential</p>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6">
+                                            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="flex items-center justify-between">
+                                    <p><span class="text-lg font-bold">12 GB</span> of storage</p>
+                                    <p class="text-sm font-bold">$12 / mo</p>
+                                </div>
+                            </div>
+                        </div>
+                    </label>
+
+                    <label class="cursor-pointer">
+                        <input type="radio" class="peer sr-only" name="pricing" />
+                        <div class="w-72 max-w-xl rounded-md bg-white p-5 text-gray-600 ring-2 ring-transparent hover:shadow peer-checked:text-sky-600 peer-checked:ring-sky-400 peer-checked:ring-offset-2">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex items-center justify-between">
+                                    <p class="text-sm font-semibold uppercase text-gray-500">essential</p>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6">
+                                            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="flex items-center justify-between">
+                                    <p><span class="text-lg font-bold">12 GB</span> of storage</p>
+                                    <p class="text-sm font-bold">$12 / mo</p>
+                                </div>
+                            </div>
+                        </div>
+                    </label>
+
+                </div>
+            </div>
             <br>
             <br>
             <br>
